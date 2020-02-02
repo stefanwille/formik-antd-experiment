bef43ca933a77e60ee0bb6b0da4dd645d1d0d747
Stefan Wille <github@stefanwille.com>
2020-02-02 10:23:46 +0100

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect } from "react";
+import { Form, InputNumber, Checkbox, Input, SubmitButton } from "formik-antd";
+import { Formik } from "formik";
+import "./App.css";
+import "antd/dist/antd.css";
+import { message } from "antd";
 
 const App = () => {
+  // tsc comile error: "'Input' refers to a value, but is being used as a type here.ts(2749)"
+  const inputRef = useRef<Input>(null);
+
+  useEffect(() => {
+    if (!inputRef.current) {
+      throw new Error("Missing ref");
+    }
+    inputRef.current.focus();
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Formik
+        initialValues={{ firstName: "", age: 20, newsletter: false }}
+        onSubmit={(values, _formikHelpers): any => {
+          message.info(JSON.stringify(values, null, 2));
+        }}
+        render={formikApi => (
+          <Form>
+            <Input name="firstName" placeholder="First Name" ref={inputRef} />
+            <InputNumber name="age" min={0} />
+            <Checkbox name="newsletter">Newsletter</Checkbox>
+            <SubmitButton>Submit</SubmitButton>
+          </Form>
+        )}
+      />
     </div>
   );
-}
+};
 
 export default App;
